c5af02bf80c55ff7de86833b9fb3725e2458b0d9
Chris Howard <info@341design.com.au>
2013-02-20 16:34:47 +1100

    removed extra js that had been commented out

--- js/cplus_metaboxes.js
+++ js/cplus_metaboxes.js
@@ -1,46 +1,6 @@
 jQuery(document).ready(function() {
 
 	 jQuery('.pizazz-meta-boxes').tabs({});
-//    var $dragsource = jQuery("div.cplus-dragsource");
-//     $dragsource.draggable({
-// 			revert:true,
-// 			revertDuration:0,
-// 			// helper:"clone"
-// stack: "#contentplus_layout-elements div",
-// cursor:"move",
-
-//       zindex:99
-
-//     });
-
-
-    // var $dropzone = jQuery( ".cplus-dropzone");
-    // $dropzone.droppable({
-    //   activeClass: ".ui-state-default",
-    //   hoverClass: ".ui-state-hover",
-    //   accept: ":not(.ui-sortable-helper)",
-    //   drop: function( event, ui ) {
-    //     var alt = ui.helper.attr('alt');
-    //     var $d = jQuery( '<span class="cplus-dropped cplus-dropped-'+alt+'" alt="%'+alt+'%"></span>' );
-    //     $d.html(ui.helper.text() ).appendTo( this );
-    //     var x = jQuery(".cell-contentplus_layout-layout").find('input[name="contentplus_layout-layout"]');
-    //     x.val(x.val() + '%' + ui.helper.attr("alt") + '% ');
-    //     ui.draggable.hide();
-    //     x.change();
-    //      // jQuery("span.cplus-dropped").resizable({
-    //      //  handles:"s",
-    //      //  ghost:true,
-    //      //  minHeight:40,
-    //      //  resize: function( event, ui ) {
-    //      //    var rs = 'contentplus_layout-'+alt+'-width';
-    //      //    console.log(rs);
-    //      //    var $r = jQuery( 'input[name="'+rs+'"]');
-    //      //    // this may be inacurate as it 's using the whole width but buthas padding'
-    //      //    $r.val(Math.round(ui.size.width/500*100));
-    //      //  }
-    //     // });
-    //  }
-    // })
     jQuery( ".cplus-dropzone").sortable({
     	cursor: "move",
       sort: function() {
@@ -49,13 +9,7 @@ jQuery(document).ready(function() {
         jQuery( this ).removeClass( "ui-state-default" );
       }
     });
-    // On disadvantage is can't size to zero
-    // jQuery('.cplus-dropped').resizable({
-    // 	handle:'e',
-    // 	maxWidth:500
-    // });
-
-
+ 
   jQuery("#pizazz-form-table-Layout input").mouseover(function(event){
   	var $source_input = jQuery(event.target);
   	var altid = $source_input.attr("alt");
