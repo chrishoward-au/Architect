e94d8f5f73691b00cbe955a8196e25e11b54ab78
Chris Howard <reg@341design.com.au>
2017-12-10 00:31:24 +1100

    1.10.8: Started work on support for AWS S3 offload
    Requires WP Offload S3 plugin

--- application/shared/architect/php/arc-constants.php
+++ application/shared/architect/php/arc-constants.php
@@ -9,14 +9,14 @@
 
   if ( ! defined( 'PZARC_TESTER' ) ) {
     $arc_options = get_option( '_architect_options', array() );
-    define( 'PZARC_TESTER', ( isset( $arc_options[ 'architect_enable_beta' ] ) ? $arc_options[ 'architect_enable_beta' ] : false ) );
+    define( 'PZARC_TESTER', ( isset( $arc_options['architect_enable_beta'] ) ? $arc_options['architect_enable_beta'] : FALSE ) );
   }
 
   define( 'PZARC_NAME', 'pzarchitect' ); // This is also same as the locale
   define( 'PZARC_FOLDER', '/pizazzwp-architect' );
   define( 'PZARC_CODEX', 'http://architect4wp.com/codex-listings' );
 
-  define( 'PZARC_BETA', false );
+  define( 'PZARC_BETA', FALSE );
 
   define( 'PZARC_PLUGIN_APP_URL', PZARC_PLUGIN_URL . 'application/' );
   define( 'PZARC_PLUGIN_APP_PATH', PZARC_PLUGIN_PATH . 'application/' );
@@ -24,22 +24,29 @@
   define( 'PZARC_DOCUMENTATION_PATH', PZARC_PLUGIN_PATH . 'documentation/' );
   define( 'PZARC_PLUGIN_ASSETS_URL', PZARC_PLUGIN_APP_URL . 'shared/assets/' );
   define( 'PZARC_PLUGIN_PRESETS_URL', PZARC_PLUGIN_URL . 'presets/' );
-  define( 'PZARC_CACHE', '/arc/' );
 
   $upload_dir = wp_upload_dir();
-  if ( substr( home_url(), 0, 5 ) === 'https' && substr( $upload_dir[ 'baseurl' ], 0, 5 ) === 'http:' ) {
-    $upload_dir[ 'baseurl' ] = str_replace( 'http:', 'https:', $upload_dir[ 'baseurl' ] );
+  if ( substr( home_url(), 0, 5 ) === 'https' && substr( $upload_dir['baseurl'], 0, 5 ) === 'http:' ) {
+    $upload_dir['baseurl'] = str_replace( 'http:', 'https:', $upload_dir['baseurl'] );
   }
-  define( 'PZARC_UPLOADS_BASEPATH', $upload_dir[ 'basedir' ] );
-  define( 'PZARC_UPLOADS_BASEURL', $upload_dir[ 'baseurl' ] );
+  define( 'PZARC_UPLOADS_BASEPATH', $upload_dir['basedir'] );
+  define( 'PZARC_UPLOADS_BASEURL', $upload_dir['baseurl'] );
 
   define( 'PZARC_PRESETS_PATH', PZARC_UPLOADS_BASEPATH . '/pizazzwp/architect/presets/' );
   define( 'PZARC_PRESETS_URL', PZARC_UPLOADS_BASEURL . '/pizazzwp/architect/presets/' );
 
-  // Need to add support for WP Offload S3
-  
-  define( 'PZARC_CACHE_URL', PZARC_UPLOADS_BASEURL . '/cache/pizazzwp/arc/' );
-  define( 'PZARC_CACHE_PATH', PZARC_UPLOADS_BASEPATH . '/cache/pizazzwp/arc/' );
+  // TODO: Need to add support for WP Offload S3
+
+  $pzarc_use_s3 = FALSE;
+
+  if ( $pzarc_use_s3 ) {
+    // Insert code
+
+  } else {
+    define( 'PZARC_CACHE', '/arc/' );
+    define( 'PZARC_CACHE_URL', PZARC_UPLOADS_BASEURL . '/cache/pizazzwp/arc/' );
+    define( 'PZARC_CACHE_PATH', PZARC_UPLOADS_BASEPATH . '/cache/pizazzwp/arc/' );
+  }
 
   // TODO: Setup an option for changing the language
   $language = substr( get_locale(), 0, 2 );
